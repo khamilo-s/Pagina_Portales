2e58c3e3fe3cd876f53d5f8f00f066ca03a11dd8
Camilo_s <54607071+khamilo-s@users.noreply.github.com>
2020-04-19 18:27:58 -0400

    Add files via upload

---
+++ Pagina_Portales/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Pagina de Portales</title>
+        <link href="css/estilo_index.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div>
+        <header id="Titulo_Pagina">
+            <h1>Tecnologia en Pistolas de Portales</h1>
+        </header>
+        </div>
+        <div>
+            <nav>
+                <a href="#"class="nav-enlace">Inicio</a>
+                <a href="#Cuerpo"class="nav-enlace">Sobre Nosotros</a>
+                <a href="#Contacto"class="nav-enlace">Contacto</a>
+            </nav>
+        </div>
+            
+        <div>
+            <section id="Empresa">
+                <p id="Titulo_cuerpo">Aperture Laboratories</p>
+                <p id="Contenido">
+                   <br> En 1952, Aperture Laboratories comienza sus operaciones como 
+                   una fabricante de cortinas de baño. Sus productos proveían 
+                   portales de baja tecnología entre el interior y el exterior 
+                   de las duchas. En realidad, hay muy poca ciencia en estos 
+                   productos: el nombre fue elegido para hacer que las cortinas 
+                   pareciesen más higiénicas. En 1956 la administración de 
+                   Eisenhower ofrece un contrato a Aperture Laboratories para 
+                   proveer cortinas de baño a todas las ramas militares, 
+                   exceptuando la Marina. Durante estos primeros años, en 
+                   secreto su fundador Cave Johnson realiza en las instalaciones
+                   subterráneas, de una antigua mina de sal, el desarrollo de 
+                   varios experimentos extravagantes que incluyen el uso de los
+                   primitivos portales, y haciendo uso de sus relaciones con el 
+                   gobierno de Estados Unidos recluta soldados y astronautas 
+                   para realizar esas peligrosas pruebas.
+                </p>
+            </section>
+        </div>
+            
+        <div>
+            <aside id="Especifica">
+                <p id="Subtitulo">Pistola de Portales</p>
+                <p id="Contenido_Pistola"> 
+                        El Dispositivo Portátil de 
+                        Portales de Aperture Science,es la herramienta 
+                        experimental diseñada para poner dos portales por los 
+                        que pueden pasar objetos.
+                        El Dispositivo Portátil de Portales está diseñado para 
+                        crear portales en cualquier superficie blanca que sea 
+                        inmóvil, plana y suficientemente grande. Cuando es 
+                        disparado, un proyectil colorado será expulsado por un
+                        barril. Si el proyectil da a una superficie válida, un 
+                        portal del color correspondiente será abierto. Si es 
+                        abierto otro portal de distinto color, ambos serán 
+                        conectados. Si un portal del mismo color es colocado, 
+                        se cerrará y se abrirá el nuevo.
+                 </p>
+                <div align="center"><img id="Pistola"src="Pistola.png"></div>
+            </aside>
+        </div>    
+        
+        
+        
+        <footer id="Contacto">
+            <p>Direccion: <a>Calle Falsa #123</a></p>
+            <p>Telefono: <a>+56959648267</a></p>
+            <p>Correo: <a>Aperturesciencespa@gmail.com</a></p>
+        </footer>
+    </body>
+</html>
